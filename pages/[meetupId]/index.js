bad4679d540e86276fdcfe3760db3931e2a88be8
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-14 18:11:20 +0200

    getStaticPaths - paths and fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -14,4 +14,42 @@ const MeetupDetails = () => {
   );
 };
 
+// returning objectw where we describe all the dynamic segment values, e.g. all meetupIds in this case
+export async function getStaticPaths() {
+  return {
+    fallback: false, // does paths contain all supported meetup Id values
+    paths: [
+      {
+        params: {
+          meetupId: "m1",
+        },
+      },
+      {
+        params: {
+          meetupId: "m2",
+        },
+      },
+    ],
+  };
+}
+
+export async function getStaticProps(context) {
+  // fetch data for a single meetup
+
+  const meetupId = context.params.meetupId;
+
+  return {
+    props: {
+      meetupData: {
+        image:
+          "https://thenomadvisor.com/wp-content/uploads/2019/04/wheretostayinvienna-780x520.jpg",
+        id: meetupId,
+        title: "Dummy Title",
+        address: "Some stree, Some City, Unknown Country",
+        description: "This is a dummy meetup",
+      },
+    },
+  };
+}
+
 export default MeetupDetails;
