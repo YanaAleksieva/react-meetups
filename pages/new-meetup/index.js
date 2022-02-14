c9e7fbd3bfa35d3e66473759c2045cac0a41449d
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-14 16:36:29 +0200

    add new meetup form

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,1 +1,13 @@
 // our-domain.com/new-meetup
+
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+const NewMeetup = () => {
+  const addMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={addMeetupHandler} />;
+};
+
+export default NewMeetup;
