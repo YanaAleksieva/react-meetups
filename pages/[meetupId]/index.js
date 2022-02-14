23189cdbee9ea6d84870d32ff241b2c3f045d499
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-14 17:13:49 +0200

    add detail component with dummy data, and css module

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,1 +1,17 @@
 // our-domain.com/[meetupId]
+
+import { Fragment } from "react";
+import MeetupDetail from "../../components/meetups/MeetupDetail";
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      image="https://thenomadvisor.com/wp-content/uploads/2019/04/wheretostayinvienna-780x520.jpg"
+      title="Dummy Title"
+      address="Some stree, Some City, Unknown Country"
+      description="This is a dummy meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
