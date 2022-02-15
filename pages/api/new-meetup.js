7bf15e160d0e5a41211c4a4347a8f98c5cc29af3
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-15 09:59:42 +0200

    connect to MongoDb

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,10 +1,26 @@
-// /api/new-meetup - if request send to this URL, it will trigger function defined below
+import { MongoClient } from "mongodb";
 
-function handler(req, res) {
+// /api/new-meetup - if request send to this URL, it will trigger function defined below
+// POST /api/new-meetup
+
+async function handler(req, res) {
   if (req.method === "POST") {
     const data = req.body;
 
-    const { title, image, address, description } = data;
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:Admin123@cluster0.xkdkl.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
+
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.insertOne(data);
+
+    console.log(result);
+
+    client.close();
+
+    res.status(201).json({ message: "Meetup inserted!" });
   }
 }
 
