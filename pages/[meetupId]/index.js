d3e7d358d3efa0dcfcf7d2a6e057f64d25636086
YanaAleksieva <ya.aleksieva@gmail.com>
2022-02-15 10:50:12 +0200

    fetch meetup details from db

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,34 +1,37 @@
 // our-domain.com/[meetupId]
 
+import { MongoClient, ObjectId } from "mongodb";
+
 import MeetupDetail from "../../components/meetups/MeetupDetail";
 
-const MeetupDetails = () => {
+const MeetupDetails = (props) => {
   return (
     <MeetupDetail
-      image="https://thenomadvisor.com/wp-content/uploads/2019/04/wheretostayinvienna-780x520.jpg"
-      title="Dummy Title"
-      address="Some stree, Some City, Unknown Country"
-      description="This is a dummy meetup"
+      image={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
 
 // returning objectw where we describe all the dynamic segment values, e.g. all meetupIds in this case
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin:Admin123@cluster0.xkdkl.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const meetups = await meetupsCollection.find({}, { _id: 1 }).toArray(); // 1st arg - {} - find all documents; 2nd arg - {_id: 1} -  only extract _id, but no ther filed values
+
+  client.close();
+
   return {
     fallback: false, // does paths contain all supported meetup Id values
-    paths: [
-      {
-        params: {
-          meetupId: "m1",
-        },
-      },
-      {
-        params: {
-          meetupId: "m2",
-        },
-      },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
@@ -37,15 +40,26 @@ export async function getStaticProps(context) {
 
   const meetupId = context.params.meetupId;
 
+  const client = await MongoClient.connect(
+    "mongodb+srv://admin:Admin123@cluster0.xkdkl.mongodb.net/meetups?retryWrites=true&w=majority"
+  );
+  const db = client.db();
+  const meetupsCollection = db.collection("meetups");
+
+  const selectedMeetup = await meetupsCollection.findOne({
+    _id: ObjectId(meetupId),
+  });
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        image:
-          "https://thenomadvisor.com/wp-content/uploads/2019/04/wheretostayinvienna-780x520.jpg",
-        id: meetupId,
-        title: "Dummy Title",
-        address: "Some stree, Some City, Unknown Country",
-        description: "This is a dummy meetup",
+        id: selectedMeetup._id.toString(),
+        image: selectedMeetup.image,
+        title: selectedMeetup.title,
+        address: selectedMeetup.address,
+        description: selectedMeetup.description,
       },
     },
   };
